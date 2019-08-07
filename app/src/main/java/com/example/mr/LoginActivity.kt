6a0fcc2ad1c3652d948cd0d58f8d6c90a29dedd3
Paper95 <qkrwlghddlek@naver.com>
2019-08-07 12:40:35 +0900

    Login

--- app/src/main/java/com/example/mr/LoginActivity.kt
+++ app/src/main/java/com/example/mr/LoginActivity.kt
@@ -1,12 +1,18 @@
 package com.example.mr
 
 import android.animation.Animator
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.os.CountDownTimer
+import android.util.Log
 import android.view.View
+import android.view.View.GONE
 import android.view.View.VISIBLE
+import android.widget.TextView
+import android.widget.Toast
 import androidx.core.content.ContextCompat
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
@@ -15,7 +21,17 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        object : CountDownTimer(3000, 1000) {
+        skipTextView.setOnClickListener{
+            afterAnimationView.visibility = GONE
+            signupView.visibility = VISIBLE
+        }
+
+        skipTextView2.setOnClickListener{
+            afterAnimationView.visibility = VISIBLE
+            signupView.visibility = GONE
+        }
+
+        object : CountDownTimer(2000, 1000) {
             override fun onFinish() {
                 bookITextView.visibility = View.GONE
                 loadingProgressBar.visibility = View.GONE
@@ -25,7 +41,66 @@ class LoginActivity : AppCompatActivity() {
             }
             override fun onTick(p0: Long) {}
         }.start()
+
+        loginButton.setOnClickListener{
+            var email = emailEditText.text.toString()
+            var passwd = passwordEditText.text.toString()
+
+            if(email.trim()=="" || passwd.trim()==""){
+                Toast.makeText(this, "공백이 존재하면 안됩니다", Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+
+            val auth = FirebaseAuth.getInstance()
+            auth.signInWithEmailAndPassword(email, passwd)
+                .addOnCompleteListener { task ->
+                    if(task.isSuccessful){
+                        var user = auth.currentUser
+                        if(user!!.isEmailVerified) {
+                            Toast.makeText(this, "로그인 성공", Toast.LENGTH_LONG).show()
+                            val nextIntent = Intent(this, MainActivity::class.java)
+                            startActivity(nextIntent)
+                        } else {
+                            Toast.makeText(this, "이메일 인증을 완료해주세요", Toast.LENGTH_LONG).show()
+                            auth.signOut()
+                        }
+                    }else{
+                        Toast.makeText(this, "로그인 실패", Toast.LENGTH_LONG).show()
+                    }
+                }
+        }
+
+
+        loginButton2.setOnClickListener{
+            var email = emailEditText2.text.toString()
+            var passwd = passwordEditText2.text.toString()
+
+            if(email.trim()=="" || passwd.trim()==""){
+                Toast.makeText(this, "공백이 존재하면 안됩니다", Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+
+
+            var domain = email.substring(email.lastIndexOf("@"))
+            if(domain == "@pusan.ac.kr")
+                doSignUp(email, passwd)
+            else
+                Toast.makeText(this, "부산대학교 메일만 사용가능합니다\nEX) email@pusan.ac.kr", Toast.LENGTH_LONG).show()
+        }
     }
+
+    fun doSignUp(email: String, passwd: String){
+        val auth = FirebaseAuth.getInstance()
+        auth.createUserWithEmailAndPassword(email, passwd)
+            .addOnCompleteListener{ task->
+                if(task.isCanceled)
+                    Toast.makeText(this, "회원가입 실패", Toast.LENGTH_LONG).show()
+                else if(task.isSuccessful)
+                    auth.currentUser!!.sendEmailVerification()
+                    Toast.makeText(this, "회원가입 성공\n인증 메일이 발송되었습니다 확인해주세요!!", Toast.LENGTH_LONG).show()
+            }
+    }
+
 
     private fun startAnimation() {
         bookIconImageView.animate().apply {
